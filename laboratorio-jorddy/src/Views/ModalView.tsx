ac3abc6457907a4aeb187231b8923cfc355fe1f4
Uluru92 <jorddy.castro@gmail.com>
2024-11-17 00:50:40 -0600

    implementacion de card and cardImage

--- laboratorio-jorddy/src/Views/ModalView.tsx
+++ laboratorio-jorddy/src/Views/ModalView.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import MensajesInformativosComponent from "../Components/MensajesInformativosComponent";
+import { Card, CardImg } from "react-bootstrap";
 
 
 const ModalView: React.FC = () =>
@@ -13,13 +14,15 @@ const ModalView: React.FC = () =>
     return (
         <div className="container">
             <h1>Ejercicio con modal</h1>
-            <button className="btn btn-warning" onClick={AbrirModalDetalle}>Quiero ver mas informacion</button>
-            <MensajesInformativosComponent
-                modalAbierto={modalAbierto}
-                setModalAbierto={setModalAbierto}
-                detalleMensaje="Consola Play 1"></MensajesInformativosComponent>
-                
-            
+            <Card>
+                <CardImg src={require('../img/playStation5.png')}></CardImg>
+                <button className="btn btn-warning" onClick={AbrirModalDetalle}>Quiero ver mas informacion</button>
+                <MensajesInformativosComponent
+                    modalAbierto={modalAbierto}
+                    setModalAbierto={setModalAbierto}
+                    detalleMensaje="Consola Play 1"></MensajesInformativosComponent>
+
+            </Card>
         </div >
     )
 }
