e4a93303516a1884e50feaa3d09f55c13f620f35
Uluru92 <jorddy.castro@gmail.com>
2024-11-17 00:04:12 -0600

    creacion del ModalView

---
+++ laboratorio-jorddy/src/Views/ModalView.tsx
@@ -0,0 +1,12 @@
+
+const ModalView: React.FC = () =>
+{
+    return (
+        <div className="container">
+            <h1>Ejercicio con modal</h1>
+
+        </div >
+    )
+}
+
+export default ModalView;
