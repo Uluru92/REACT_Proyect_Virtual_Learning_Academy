a16f2e2887fe5d81370bf8f9a999b765db463752
Uluru92 <jorddy.castro@gmail.com>
2024-11-17 00:58:36 -0600

    card e imagenes implemented

--- laboratorio-jorddy/src/Views/ModalView.tsx
+++ laboratorio-jorddy/src/Views/ModalView.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import '../css/estilosConsolas.css'
 import MensajesInformativosComponent from "../Components/MensajesInformativosComponent";
 import { Card, CardImg } from "react-bootstrap";
 
@@ -14,14 +15,31 @@ const ModalView: React.FC = () =>
     return (
         <div className="container">
             <h1>Ejercicio con modal</h1>
-            <Card>
+            <Card className="estiloContenedorConsola">
                 <CardImg src={require('../img/playStation5.png')}></CardImg>
                 <button className="btn btn-warning" onClick={AbrirModalDetalle}>Quiero ver mas informacion</button>
                 <MensajesInformativosComponent
                     modalAbierto={modalAbierto}
                     setModalAbierto={setModalAbierto}
                     detalleMensaje="Consola Play 1"></MensajesInformativosComponent>
+            </Card>
 
+            <Card className="estiloContenedorConsola">
+                <CardImg src={require('../img/xbox.png')}></CardImg>
+                <button className="btn btn-warning" onClick={AbrirModalDetalle}>Quiero ver mas informacion</button>
+                <MensajesInformativosComponent
+                    modalAbierto={modalAbierto}
+                    setModalAbierto={setModalAbierto}
+                    detalleMensaje="xbox"></MensajesInformativosComponent>
+            </Card>
+
+            <Card className="estiloContenedorConsola">
+                <CardImg src={require('../img/nintendo.png')}></CardImg>
+                <button className="btn btn-warning" onClick={AbrirModalDetalle}>Quiero ver mas informacion</button>
+                <MensajesInformativosComponent
+                    modalAbierto={modalAbierto}
+                    setModalAbierto={setModalAbierto}
+                    detalleMensaje="nintendo"></MensajesInformativosComponent>
             </Card>
         </div >
     )
