937948775ddea862ff4fb392708cd482a6f13ebc
Uluru92 <jorddy.castro@gmail.com>
2024-11-17 00:17:37 -0600

    show and hide en modal

--- laboratorio-jorddy/src/Components/MensajesInformativosComponent.tsx
+++ laboratorio-jorddy/src/Components/MensajesInformativosComponent.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { Modal, ModalBody, ModalHeader, ModalTitle } from "react-bootstrap";
 
-interface MensajesInformativosComponentProps{ //Asi se genera/dispara un evento modal
+interface MensajesInformativosComponentProps//Asi se genera/dispara un evento modal
+{ 
     modalAbierto: boolean, //Este garantiza el estado
     setModalAbierto: React.Dispatch<React.SetStateAction<boolean>>; //este lo habilita
 }
 
-const MensajesInformativosComponent: React.FC<MensajesInformativosComponentProps> = () =>{
-        return <div>
-            <Modal>
+const MensajesInformativosComponent: React.FC<MensajesInformativosComponentProps> = ({modalAbierto,setModalAbierto}) =>{
+    
+    function cerrarModal()
+    {
+        setModalAbierto(false);
+    }
+    
+    return <div>
+            <Modal
+                show={modalAbierto}
+                onHide={cerrarModal}>
                 <ModalHeader>
                     <ModalTitle>Detalle del modal</ModalTitle>
                 </ModalHeader>
