1ed5ef84732baba81b13fa81b499a28dd275ca80
Uluru92 <jorddy.castro@gmail.com>
2024-11-17 13:14:02 -0600

    consumer y provider context view

---
+++ laboratorio-jorddy/src/Views/HookUseContextConsumerView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HookuseContextConsumerView: React.FC = () => {
+    return (
+        <div>
+
+        </div >
+    )
+}
+
+export default HookuseContextConsumerView;
---
+++ laboratorio-jorddy/src/Views/HookUseContextProvideView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const HookuseContextProviderView: React.FC = () => {
+    return (
+        <div>
+
+        </div >
+    )
+}
+
+export default HookuseContextProviderView;
