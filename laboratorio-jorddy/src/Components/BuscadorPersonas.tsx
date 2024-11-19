1eb256ef156a085550ce6357d374b3737d9a6be3
Uluru92 <jorddy.castro@gmail.com>
2024-11-19 10:27:10 -0600

    hook useMemo  implemented con filtros

--- laboratorio-jorddy/src/Components/BuscadorPersonas.tsx
+++ laboratorio-jorddy/src/Components/BuscadorPersonas.tsx
@@ -1,21 +1,43 @@
+import { useMemo, useState } from "react";
+
 const BuscadorPersonas: React.FC = () =>
 {
-    const user = [
-        { id: 1, name: 'adolfo' }
-        { id: 2, name: 'Caleb' }
-        { id: 3, name: 'Alison' }
-        { id: 4, name: 'Josue' }
-        { id: 5, name: 'Jorddy' }
-        { id: 6, name: 'Sophia' }
-        { id: 7, name: 'Angie' }
-        { id: 8, name: 'Samuel' }
-        { id: 9, name: 'Natalia' }
-        { id: 10, name: 'Leticia' }
+    const users = [
+        { id: 1, name: 'adolfo vargas' },
+        { id: 2, name: 'Caleb' },
+        { id: 3, name: 'Alison' },
+        { id: 4, name: 'Josue' },
+        { id: 5, name: 'Jorddy' },
+        { id: 6, name: 'Sophia' },
+        { id: 7, name: 'Angie' },
+        { id: 8, name: 'Samuel' },
+        { id: 9, name: 'Natalia' },
+        { id: 10, name: 'Leticia' },
+        { id: 11, name: 'adolfo sandoval' }
     ]
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const filteredUsers = useMemo(() => {
+        return users.filter(user => user.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    }, [searchTerm]);
 
     return (
         <div>
             <h1>Vamos a filtrar personas</h1>
+            <input
+                className="form-control"
+                placeholder="buscar empleados..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}></input>
+            <br /><br />
+            <ul>
+                {
+                    filteredUsers.map(user => (
+                        <li key={user.id}>{user.name}</li>
+                    ))
+                }
+            </ul>
         </div >
     )
 }
